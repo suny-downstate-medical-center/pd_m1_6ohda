a7e1de5ea6b34eca8bc616e5c6a63dcc334cf1e5
Bill Lytton <wwlytton@yahoo.com>
2024-08-29 20:13:47 -0400

    assuming that i'm reading gmgs datafile correctly i see that he has only 6 in the basilar dend and 364 apical!  -- Length: 360; Range: -19.8462 (6) to 1430.15 (354)

--- ancill.py
+++ ancill.py
@@ -1,8 +1,9 @@
 from wlutils import flattn
 import numpy as np
+import json
 
 def mvTL_PT ():
     with open('./sim/conn/conn_dend_PT.json') as f: ptconn=json.load(f)
     locs = np.array(flattn([y for x in ptconn.values() if type(x) is dict for y in x.values()]))
-
-
+    rlocs = np.array([x-ptconn['fixedSomaY'] for x in locs])
+    print("Length: %d; Range: %g (%d) to %g (%d)\n"%(len(rlocs),rlocs.min(),np.sum(rlocs<0),rlocs.max(),np.sum(rlocs>0)))
