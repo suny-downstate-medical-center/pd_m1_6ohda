89eb758ba9b6b0e95f6ba2da18688a3e87148f0f
Don Doherty 412 478 4552 <don@zn.neurosim.downstate.edu>
2024-09-07 21:47:44 -0400

    line added to init.py for running on Neurosim machines

--- sim/init.py
+++ sim/init.py
@@ -18,6 +18,6 @@ from netpyne import sim
 from cfg import cfg
 from netParams import netParams
 
-sim.pc.timeout(300)
+sim.pc.timeout(300) # needed to run on Neurosim machines
 sim.createSimulateAnalyze(netParams, cfg)
 
